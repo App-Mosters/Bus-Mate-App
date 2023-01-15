23b1608325f909afad217091a5bc8630b54037ce
kevthebrunton <s92068815@ousl.lk>
2023-01-15 20:28:46 +0530

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,82 @@
+import { StyleSheet, Text, View,
+  TouchableOpacity,Image, ImageBackground, BackHandler,
+  Linking} from 'react-native';
+import React,{Component,Fragment} from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Home = () => {
+    const Tab = createBottomTabNavigator();
+
+    function HomeScreen() {
+        return (
+          //Home screen code
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+            
+          </View>
+
+        );
+      }
+      
+      function Wallet() {
+        return (
+          //wallet screen code
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Wallet!</Text>
+          </View>
+        );
+      }
+
+      function Profile() {
+        return (
+          //profile screen code
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Profile!</Text>
+          </View>
+        );
+      }
+      
+  return (
+    
+      <Tab.Navigator>
+        <Tab.Screen 
+        name= "Home"
+        component={HomeScreen} 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }} />
+        <Tab.Screen 
+        name="Wallet" 
+        component={Wallet} 
+        options={{
+        tabBarLabel: 'Wallet',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="wallet" color={color} size={size} />
+            ),
+          }}/>  
+        <Tab.Screen 
+        name="Profile" 
+        component={Profile} 
+        options={{
+          tabBarLabel: 'Profile',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="face-man-profile" color={color} size={size} />
+              ),
+            }}/>
+      
+      </Tab.Navigator>
+    
+  )
+}
+
+export default Home;
+
+//const styles = StyleSheet.create({
+  
+//});
